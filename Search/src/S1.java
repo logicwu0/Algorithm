a4233fbcffcddb4f113be59b76f9545c05fe00ce
logicwu0 <605524858@qq.com>
2018-03-10 20:10:50 +0800

    add

---
+++ Search/src/S1.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+/**
+ * Search for a Range
+ * For example,Given [5,7,7,8,8,10] and target value 8,return [3,4]
+ * not found in the array return [-1,-1]
+ *
+ * */
+//已经排好序的  用二分查找法
+public class S1 {
+
+    public static int[] searchRange(int[] nums, int target) {
+        int lower = lower_bound(nums, 0, nums.length, target);
+        int upper = upper_bound(nums, 0, nums.length, target);
+
+        if (lower == nums.length || nums[lower] != target) {
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{lower, upper - 1};
+        }
+    }
+        static int lower_bound(int[] A, int first,int last,int target){
+            while (first != last) {
+                int mid = first + (last - first) / 2;
+                if (target > A[mid]) {
+                    first = ++mid;
+                } else {
+                    last = mid;
+                }
+            }
+            return first;
+        }
+        static int upper_bound ( int[] A, int first, int last, int target){
+            while (first != last) {
+                int mid = first + (last - first) / 2;
+                if (target >= A[mid]) {
+                    first = ++mid;
+                } else {
+                    last = mid;
+                }
+            }
+            return first;
+        }
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{5,7,7,8,8,10};
+        int target = 8;
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+
+    }
+}
---
+++ Sort/src/S1.java
@@ -0,0 +1,16 @@
+/**
+ * Insertion Sort List
+ * 插入排序
+ *
+ *
+ * */
+public class S1 {
+
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
