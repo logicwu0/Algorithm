3432429f4b81edfb015d37126cce2e9ff0d1a68c
logicwu0 <605524858@qq.com>
2018-09-20 14:51:42 +0800

    mod

--- String/src/S3.java
+++ String/src/S3.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
 
 public class S3 {
+    private static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int res = 0;
+        HashSet set = new HashSet();
+        for(int i = 0; i < s.length(); i++) {
+            if(!set.add(s.charAt(i))) {
+                res = 0;
+            }else {
+                res++;
+                max = Math.max(res,max);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+       String s = "pwwkew";
+       System.out.println(lengthOfLongestSubstring(s));
+    }
 }
